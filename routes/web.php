be3eff99025b535fabf9670da21de3eac83e8ee3
KUI\ahkui <ahkui@outlook.com>
2017-09-02 19:14:03 +0800

    init

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::domain('{account}.ahkui.com')->group(function () {
+    Route::get('user/{id?}', function ($account, $id) {
+        dd($account, $id);
+    });
+});

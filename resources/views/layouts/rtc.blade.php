f254ac408d91d48f10bf810a06643daefc684ec2
KUI\ahkui <ahkui@outlook.com>
2017-09-04 04:23:05 +0800

    rtc template apply

---
+++ resources/views/layouts/rtc.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app') 
+@section('nav') 
+    @include('components.rtc.nav') 
+@endsection 
+@section('footer') 
+    @include('components.rtc.footer') 
+@endsection 
+@section('content') 
+    @yield('content') 
+@endsection 
+@section('hidden-content') 
+    @yield('hidden-content')
+<script>
+function Load_with_axios($this) {
+    axios.get($($this).data('href'))
+        .then(function(response) {
+            $("html, body").stop().animate({ scrollTop: 0 }, 350, 'swing');
+            $('.nav-item.active , .nav-link.active,.dropdown-item.active').removeClass('active');
+            if ($($this).hasClass('dropdown-item')) {
+                $($this).parent().parent().find('.nav-link.dropdown-toggle').addClass('active');
+                $($this).addClass('active');
+            } else {
+                $($this).addClass('active');
+            }
+            $('#content').stop().animate({ opacity: 0 }, 200, 'swing', function() {
+                $('#content').html(response.data.html).animate({ opacity: 1 }, 200, 'swing', function() {
+                    if (response.data.run) RTC[response.data.run]();
+                })
+            });
+            document.getElementsByTagName("title").innerHTML = response.data.title;
+            window.history.pushState(response.data, "", $($this).data('href'));
+        })
+        .catch(function(error) {});
+    event.preventDefault();
+    return false;
+}
+window.onpopstate = function(e) {
+    if (e.state) {
+        document.getElementById("content").innerHTML = e.state.html;
+        document.getElementsByTagName("title").innerHTML = e.state.title;
+    }
+};
+</script>
+
+@endsection 
+@push('scripts')
+<script src="{{asset('js/rtc.js')}}"></script>
+
+@endpush 
+@push('css')
+<link href="{{asset('css/rtc.css')}}" rel="stylesheet"></link>
+
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,28 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    if (request()->ajax()) return response()->json(['html'=>view('home')->with(['title'=>'Home'])->render(),'title'=>'Home']);
+    return view('home')->with(['title'=>'home']);
+})->name('rtc.home');
+
+Route::get('about', function () {
+    if (request()->ajax()) return response()->json(['html'=>view('about')->with(['title'=>'about'])->render(),'title'=>'about']);
+    return view('about')->with(['title'=>'about']);
+})->name('rtc.about');
+
+Route::get('/portfolio', function () {
+    if (request()->ajax()) return response()->json(['html'=>view('portfolio')->with(['title'=>'portfolio'])->render(),'title'=>'portfolio']);
+    return view('portfolio')->with(['title'=>'portfolio']);
+})->name('rtc.portfolio');
+
+Route::get('/contact', function () {
+    if (request()->ajax()) return response()->json(['html'=>view('contact')->with(['title'=>'contact'])->render(),'title'=>'contact','run'=>'contact_me']);
+    return view('contact')->with(['title'=>'contact']);
+})->name('rtc.contact');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
 
 Route::domain('club.www.ahkui.com')->group(function () {
     Route::get('user/{id?}', function ($id) {

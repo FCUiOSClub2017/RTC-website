f6320005103a378f294ca9d816b3fb9dba001622
KUI\ahkui <ahkui@outlook.com>
2017-09-11 23:44:38 +0800

    create welcome page

---
+++ resources/views/welcome/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{app()->getLocale()}}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="csrf-token" content="{{csrf_token()}}">
+    <title>宣傳頁面</title>
+</head>
+
+<body>
+
+    <a href="{{route('iosclub.home')}}"></a>
+</body>
+
+</html>

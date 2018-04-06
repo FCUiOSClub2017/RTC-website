ac94f451f591f27dd11c37e58caf0d1879bae971
KUI\ahkui <ahkui@outlook.com>
2018-04-06 20:06:53 +0800

    update schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('deploy')
+        $schedule->command('git:deploy')
                  ->everyMinute();
     }
 

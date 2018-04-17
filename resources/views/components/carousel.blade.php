05467dc3911950eb19af2ee21222158541fc1542
KUI\ahkui <ahkui@outlook.com>
2018-04-17 16:01:17 +0800

    add link to banner

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -13,7 +13,7 @@
             <img class="d-block w-100" src="img\banner\03.jpg">
         </div>
         <div class="carousel-item active">
-            <img class="d-block w-100" src="img\banner\04.jpg">
+            <img class="d-block w-100" src="img\banner\04.jpg" onclick="window.location.href='https://rtc-2018.fcu.edu.tw/'" style="cursor: pointer;">
         </div>
     </div>
     <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">

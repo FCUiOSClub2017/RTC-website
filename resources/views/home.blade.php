8aa79ab61b67e556b6f580cc503de96087997178
xXHerngXx <37330413+xXHerngXx@users.noreply.github.com>
2019-03-31 11:09:53 +0800

    temp remove teacher section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends("layouts.".((request()->ajax())?'axios':'app')) @section('content') 
 <div id="home">
   @include('components.carousel') 
-  @include('components.teacher')
+{{--  @include('components.teacher') --}}
   @include('components.intro') 
 {{-- @include('components.course') --}}
 </div>

d06075ca757ee79d4f046b517a917b159ecf0910
xRockBoyx <kp3344567@gmail.com>
2021-03-12 04:29:23 +0800

    Fix routing

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,13 +10,20 @@ require('./bootstrap');
 window.Vue = require('vue');
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
+import home from './components/Home.vue';
+import headerz from './components/Header.vue';
+import footerz from './components/Footer.vue';
+import finalcut from './components/FinalCutPro.vue';
+import garageband from './components/Garageband.vue';
+import imove from './components/LogicPro.vue';
+import logicpro from './components/LogicPro.vue';
 
 Vue.component('home', require('./components/Home.vue').default)
 Vue.component('headerz', require('./components/Header.vue').default)
 Vue.component('footerz', require('./components/Footer.vue').default)
 Vue.component('finalcut', require('./components/FinalCutPro.vue').default)
 Vue.component('garageband', require('./components/Garageband.vue').default)
-Vue.component('imove', require('./components/Imove.vue').default)
+Vue.component('imove', require('./components/LogicPro.vue').default)
 Vue.component('logicpro', require('./components/LogicPro.vue').default)
 
 /**
@@ -28,5 +35,6 @@ Vue.component('logicpro', require('./components/LogicPro.vue').default)
 // Vue.component('example', require('./components/Example.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    components: { home,headerz,footerz, finalcut, garageband, imove, logicpro }
 });

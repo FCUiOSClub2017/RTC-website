8fdb1ba4d8bac4f74dd6eaa78365c826c3367447
xRockBoyx <kp3344567@gmail.com>
2021-03-12 04:54:26 +0800

    Hot Fix.

--- resources/views/lessons/index.blade.php
+++ resources/views/lessons/index.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-
+<head>
+    <title>推廣處課程</title>
+    <link rel="icon" type="image/png" href="/img/FCU_LOGO.png" />
+</head>
     <div id="app">
         <headerz></headerz>
         <home></home>
